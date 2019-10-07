7fe637b12aa895738fa901bfa839d89d572525f7
ValerioZucoloto <55113492+ValerioZucoloto@users.noreply.github.com>
2019-10-06 21:49:14 -0300

    Add files via upload
    Nessa atualização foi elaborada a comunicação entre as classes Cliente e Plataforma.

--- Cliente.cs
+++ Cliente.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 
 class Cliente {
         public string Nome {get; set;}
         public string Cpf {get; set;}
         public int Idade {get; set;}
         public Plataforma Plat;
+        public List <Plataforma> recursos;
 
         public Cliente (string nome, string cpf, int idade, Plataforma plataforma) {
                 Nome = nome;
@@ -17,12 +19,26 @@ class Cliente {
 
         }
 
-         public string exibe()  {
+		public Cliente (string nome) {
+			Nome = nome;
+		}
+
+        public void AddPlataforma(Plataforma plataforma) {
+                recursos.Add (plataforma);
+        }
+
+        public void exibirPlataforma() {
+                foreach (Plataforma obj in recursos){
+                Console.WriteLine(obj.exibe());
+                Console.WriteLine();
+                }
+        }
+
+         public string exibe(){
                 return "\nNome: " + Nome 
                 + "\nCPF: " + Cpf
                 + "\nIdade: " + Idade
-                + "\nPlataforma: " + Plat.exibe();
-                
+                + "\nPlataforma: " + Plat.exibe();  
         }
 
 }
--- main.cs
+++ main.cs
@@ -1,16 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class MainClass {
   public static void Main (string[] args) {
     //http://codepad.org/VwIDTKG8
     //tiny.cc/miniprojaed
 
-        Plataforma p1 = new Plataforma("JLG", 40.23, "1250AJP", 227);
+        List <Plataforma> plataforma = new List <Plataforma>();
+        Cliente Valerio = new Cliente ("Valério");
+	Plataforma plat1 = new Plataforma ("450AJ", Valerio);
+	Plataforma plat2 = new Plataforma ("600AJ", Valerio);
+	Plataforma plat3 = new Plataforma ("800AJ", Valerio);
+	Plataforma plat4 = new Plataforma ("1250AJP", Valerio);
 
-        Cliente cliente1 = new Cliente("Valerio", "147147147069", 24, p1);
+	plataforma.Add (plat1);
+	plataforma.Add (plat2);
+	plataforma.Add (plat3);
+	plataforma.Add (plat4);
 
-        Console.WriteLine(cliente1.exibe());
+	Console.WriteLine ("Escolha a plataforma: ");
+		for(int i = 0; i < plataforma.Count; i++){
+			Console.WriteLine("Digite " + i + " para escolher: " + plataforma[i].Modelo);
+		}
 
+	Console.WriteLine();
+
+	Console.Write ("Escolha uma das opções acima: ");
+	int escolha = int.Parse(Console.ReadLine());
+
+	if(escolha < 0 || escolha > 3){
+		Console.WriteLine("Opção Inválida!");
+		        }else{
+			if(escolha == 0){
+			        Console.WriteLine ("Cliente: " + plat1.ClienteObj.Nome);
+			        Console.WriteLine ("Plataforma escolhida: 450AJ !");
+		        }else if(escolha == 1){
+			        Console.WriteLine ("Plataforma escolhida: 600AJ !");
+		        }else if(escolha == 2){
+			        Console.WriteLine ("Plataforma escolhida: 800AJ !");
+		        }else if(escolha == 5){
+			        Console.WriteLine ("Plataforma escolhida: 1250AJP !");
+			}
+		}
     
   }
 }
